e16107e1cb41e471d831a14de6fee9171a274aa0
lmonsalve <lmonsalve@webschool.org>
2020-03-11 12:32:58 +0100

    modificando sw

--- sw.js
+++ sw.js
@@ -4,7 +4,14 @@ const STATIC_CACHE = "static-v1";
 const INMUTABLE_CACHE = "inmutable-v1";
 const DYNAMIC_CACHE = "dynamic-v1";
 
-const APP_SHELL = ["/", "index.html", "css/style.css", "img/favicon.ico", "js/app.js", "js/sw-utils.js"];
+const APP_SHELL = [
+  // "/",
+  "index.html",
+  "css/style.css",
+  "img/favicon.ico",
+  "js/app.js",
+  "js/sw-utils.js"
+];
 
 const APP_SHELL_INMUTABLE = [
   "https://fonts.googleapis.com/css?family=Quicksand:300,400",
